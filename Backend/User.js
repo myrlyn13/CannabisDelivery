2928776b324c211de583ee7665e4bc9b1945c6d6
myrlyn13 <myranda.lynne13@gmail.com>
2024-12-11 20:14:26 -0500

    User.js

---
+++ Backend/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  medicalCardNumber: { type: String, required: true },
+  medicalCardExpiry: { type: Date, required: true },
+  isVerified: { type: Boolean, default: false },
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
